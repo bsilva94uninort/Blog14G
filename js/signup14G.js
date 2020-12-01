9d39ec9b8a168acf6d422fbac18b409e16dc206e
MarcelDeLavalle <marceld@uninorte.edu.co>
2020-12-01 12:28:08 -0500

    Version #4: pagina de registro (sign up)

---
+++ js/signup14G.js
@@ -0,0 +1,14 @@
+function validarFormulario(){
+    let usuario = document.formulario.nombreUsuario;
+    let contrasena = document.formulario.pass;
+    do{
+        if(usuario.value.length==0 || usuario.value.length<5){
+            alert("El usuario debe tener mínimo 5 caracteres");
+            return false}
+        if(contrasena.value.length==0 || contrasena.value.length<10){
+            alert("La contraseña debe tener mínimo 10 caracteres");
+            return false}
+    }while(false)
+    return alert("Se ha enviado correo de autenticación a "+document.formulario.email.value)
+}
+
